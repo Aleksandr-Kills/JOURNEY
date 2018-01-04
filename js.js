48c89266e4fa2236e88f3373c513b9ddf5ea3b9c
JAY <+>
2018-01-04 16:19:24 +0200

    content-one

--- js.js
+++ js.js
@@ -1,7 +1,8 @@
+// burger-menu
 $('.main-item').click(function(){
     $('.navigation').fadeToggle();
 });
-
+// slider
 jQuery('.slider').slick ({
     dots: false,
     arrows:true,
@@ -9,4 +10,23 @@ jQuery('.slider').slick ({
     speed: 2000,
     draggable: false,
     fade:true
+});
+// calendar
+$(function(){
+    var data = [
+        {"date": "2018-01-17 15:30:30", "title": "Событие 1", "description": "Lorem Ipsum dolor set", "url": "http://www.event3.com/"}
+    ];
+    $('#eventCalendar').eventCalendar({
+        jsonData: data,
+        jsonDateFormat: 'human',
+        locales: {
+            locale: "en",
+            moment: {
+                "months": ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE",
+                    "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"],
+                "monthsShort": ["JAN", "FEB", "MAR", "APR", "MAY", "JUN",
+                    "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
+            }
+        }
+    });
 });
